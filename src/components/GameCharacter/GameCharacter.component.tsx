bcc08a0574d815be88d7e88bcf1b7ef01602a90c
harrison-symes <symeshjb@gmail.com>
2018-12-27 22:39:53 +1300

    switch position of stats and portrait for enemy character

--- src/components/GameCharacter/GameCharacter.component.tsx
+++ src/components/GameCharacter/GameCharacter.component.tsx
@@ -10,13 +10,13 @@ class GameCharacter extends React.Component<GameCharacterProps> {
         const {isPlayerSide} = this.props
 
         return (
-            <div className="game-character--container">
+            <div className={`game-character--container ${
+                    !isPlayerSide && "flex-reverse"
+                }`}>
                 <div className="game-character--portrait-container">
                     <img className="game-character--portrait" src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1eT_cT2zIf1_X1jh8dUbm-QSoWq8VJP37MtKqq0rhv_iWFwcc"} />
                 </div>
-                <div className={`game-character--stats ${
-                    !isPlayerSide && "flex-reverse"
-                }`}>
+                <div className="game-character--stats">
                     Stats here
                 </div>
             </div>
