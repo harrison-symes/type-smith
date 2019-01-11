9ed9f0f0968dd68392daf14d9ef891ae99ae029d
harrison-symes <symeshjb@gmail.com>
2019-01-11 23:50:18 +1300

    append styles to register page

--- src/components/Auth/Register.component.tsx
+++ src/components/Auth/Register.component.tsx
@@ -33,31 +33,31 @@ class Register extends React.Component<
         const { auth } = this.props
         return (
             <div className="center">
-                <h2 className="page-title">Register</h2>
                 <form onSubmit={this.submit} className="form">
                     {auth.errorMessage && 
                         <span>{auth.errorMessage}</span>
                     }
 
-                    <label htmlFor="user_name">Username</label>
-                    <input required placeholder="User Name" type="text" name="user_name" onChange={this.updateDetails} />
+                    <label className="mt3" htmlFor="user_name">Username</label>
+                    <input className="mt2" required placeholder="User Name" type="text" name="user_name" onChange={this.updateDetails} />
 
-                    <label>Password</label>
-                    <input required placeholder="Password" type="password" name="password" onChange={this.updateDetails} />
+                    <label className="mt3">Password</label>
+                    <input className="mt2" required placeholder="Password" type="password" name="password" onChange={this.updateDetails} />
                     
-                    <label>Confirm Password</label>
-                    <input required placeholder="Confirm Password" type="password" name="confirm_password" onChange={this.updateDetails} />
+                    <label className="mt3">Confirm Password</label>
+                    <input className="mt2" required placeholder="Confirm Password" type="password" name="confirm_password" onChange={this.updateDetails} />
 
-                    <input className="btn btn--green" value="Register" type="submit" />
+                    <input className="btn" value="Register" type="submit" />
+
+                    <span className="info-text">
+                        Already have an account?
+                        {" "}
+                        <Link to="/">
+                            Sign in
+                        </Link>
+                    </span>
 
                 </form>
-                <span className="info-text">
-                    Already have an account?
-                    {" "}
-                    <Link to="/">
-                        Sign in
-                    </Link>
-                </span>
             </div>
         )
     }
