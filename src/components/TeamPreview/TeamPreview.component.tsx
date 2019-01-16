76e10719ecefb9473ec0544b072f038e1466ad0a
harrison-symes <symeshjb@gmail.com>
2019-01-16 19:40:03 +1300

    shift team building control logic to preGame container

--- src/components/TeamPreview/TeamPreview.component.tsx
+++ src/components/TeamPreview/TeamPreview.component.tsx
@@ -14,91 +14,10 @@ import { Progress } from 'react-sweet-progress';
 
 export interface TeamPreviewProps {
     socket: Socket,
-    gameInfo: GameState
-}
-
-const characters = [
-    {
-        characterClass: CharacterClassList.WARRIOR,
-        abilities: [
-            { name: GAME_ATTACKS.DEMOLISH },
-            { name: GAME_ATTACKS.SLASH },
-            { name: GAME_ATTACKS.RECKLESS_SLAM },
-            { name: GAME_ATTACKS.TANK_UP }
-        ]
-    },
-    {
-        characterClass: CharacterClassList.MAGE,
-        abilities: [
-            { name: GAME_ATTACKS.METEOR },
-            { name: GAME_ATTACKS.FIREBALL },
-            { name: GAME_ATTACKS.MOLTEN_CORE },
-            { name: GAME_ATTACKS.FROST_ARMOUR },
-        ]
-    },
-    {
-        characterClass: CharacterClassList.ASSASSIN,
-        abilities: [
-            { name: GAME_ATTACKS.ASSASSINATE },
-            { name: GAME_ATTACKS.BACKSTAB },
-            { name: GAME_ATTACKS.RECUPERATE },
-            { name: GAME_ATTACKS.ACCELERATE }
-        ]
-    },
-    {
-        characterClass: CharacterClassList.PALADIN,
-        abilities: [
-            { name: GAME_ATTACKS.SANCTUARY },
-            { name: GAME_ATTACKS.BLESSED_HAMMER },
-            { name: GAME_ATTACKS.HOLY_RADIANCE },
-            { name: GAME_ATTACKS.INSPIRE },
-        ]
-    },
-    {
-        characterClass: CharacterClassList.WITCH,
-        abilities: [
-            { name: GAME_ATTACKS.WITCHING_HOUR },
-            { name: GAME_ATTACKS.BLOOD_MOON },
-            { name: GAME_ATTACKS.CURSE },
-            { name: GAME_ATTACKS.ENTRAP },
-        ]
-    },
-    {
-        characterClass: CharacterClassList.SNIPER,
-        abilities: [
-            { name: GAME_ATTACKS.RAPID_FIRE },
-            { name: GAME_ATTACKS.PIERCING_SHOT },
-            { name: GAME_ATTACKS.RELOAD },
-            { name: GAME_ATTACKS.SPIKE_TRAP },
-        ]
-    },
-] as Partial<Character>[]
-
-const icons = {
-    [CharacterClassList.WARRIOR]: "ra-crossed-swords",
-    [CharacterClassList.MAGE]: "ra-wizard-face",
-    [CharacterClassList.ASSASSIN]: "ra-cowled",
-    [CharacterClassList.PALADIN]: "ra-elf-helmet",
-    [CharacterClassList.WITCH]: "ra-witch-face",
-    [CharacterClassList.SNIPER]: "ra-eye-target",
-}
-
-const passives = {
-    [CharacterClassList.WARRIOR]: "The Warriors attacks have their power increased by 1% for each point of health the Warrior is missing",
-    [CharacterClassList.MAGE]: "The Mage gains +1 Power at the end of each turn while Active.",
-    [CharacterClassList.ASSASSIN]: "The Assassin has +6 Priority when switching",
-    [CharacterClassList.PALADIN]: "The Paladin restores 1 Health to themselves at the end of each turn while Active.",
-    [CharacterClassList.WITCH]: "All of the Witch's attacks also lower the opponent's Defense by 1",
-    [CharacterClassList.SNIPER]: "The Sniper gains +1 bonus Energy at the end of each turn, no matter where they are."
-}
-
-const classDescriptions = {
-    [CharacterClassList.WARRIOR]: "A Brutal attacker. The Warrior can efficiently deal heavy amounts of damage to opponents.",
-    [CharacterClassList.MAGE]: "Strong power but weak defenses. The Mage can boost their power to deal devastating attacks.",
-    [CharacterClassList.ASSASSIN]: "Fast and fragile. The Assassin can get off some quick attacks to finish weakened enemies, or power up to run away with the game.",
-    [CharacterClassList.PALADIN]: "Slow and Sturdy. The Paladin can heal and buff their teamates, but deals little damage themselves.",
-    [CharacterClassList.WITCH]: "Nasty and tricky. The Witch can lower opponent's stats, prevent switching, and turn their enemies own power against them",
-    [CharacterClassList.SNIPER]: "The Sniper can shoot through opponent's defense, build up to their ultimate quickly, which is a devastating attack in itself. They can also spike trap opponents, causing them to take damage when switching out."
+    gameInfo: GameState,
+    selectedTeam: Character[],
+    characters: Character[],
+    selectCharacter (character: Character) : void;
 }
 
 interface TeamPreviewState {
@@ -106,43 +25,7 @@ interface TeamPreviewState {
     activeCharacter: Partial<Character>,
 }
 
-const teamSize = 4 || 5
-
 class TeamPreview extends React.Component<TeamPreviewProps, TeamPreviewState> {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            selectedTeam: [],
-            activeCharacter: null
-        }
-    }
-    selectCharacter = (character) => {
-        let {selectedTeam} = this.state
-
-        if (selectedTeam.find(selected => selected.characterClass == character.characterClass)) {
-            selectedTeam = selectedTeam.filter(selected => selected.characterClass != character.characterClass)
-
-        } else if (selectedTeam.length < teamSize) {
-            selectedTeam.push(character)
-        }
-        this.setState({selectedTeam})
-    }
-    submitTeam = () => {
-        const {socket, gameInfo} = this.props
-        const {selectedTeam, activeCharacter} = this.state
-
-        const idx = selectedTeam.findIndex(c => c.characterClass == activeCharacter.characterClass)
-        
-        selectedTeam[idx].isActive = true
-
-        socket.emit(
-            TEAM_PREVIEW_SOCKET_CHANNEL.SUBMIT_TEAM,
-            gameInfo.roomId,
-            gameInfo.user_id,
-            selectedTeam
-        )
-    }
     renderStatBars = characterClass => {
         const stat = (label, stat) => (
             <Progress
@@ -165,31 +48,28 @@ class TeamPreview extends React.Component<TeamPreviewProps, TeamPreviewState> {
 
         )
     }
-    renderTooltip = character => {
-        return <span>
-            <div>{classDescriptions[character.characterClass]}</div>
-            <hr />
-            <div>{passives[character.characterClass]}</div>
-        </span>
-    }
+    // renderTooltip = character => {
+    //     return <span>
+    //         <div>{classDescriptions[character.characterClass]}</div>
+    //         <hr />
+    //         <div>{passives[character.characterClass]}</div>
+    //     </span>
+    // }
     render() {
-        const {selectedTeam, activeCharacter} = this.state
-
+        const {selectedTeam, characters, selectCharacter} = this.props
+        
         return (
-            <div>
-                <h1 className="page-title">Team Preview</h1>
-                
                 <div className="character-roster">
                     {characters.map(character => (
                         <div className={`character-roster--item ${
                             !!selectedTeam.find(selected => selected.characterClass == character.characterClass) && "selected"
                         }`}>
-                            <div className="portrait" onClick={() => this.selectCharacter(character)}>
+                            <div className="portrait" onClick={() => selectCharacter(character)}>
                                 <div className="portrait--inner">
 
                                 </div>
                             </div>
-                            <div className="name-bar" onClick={() => this.selectCharacter(character)}>
+                            <div className="name-bar" onClick={() => selectCharacter(character)}>
                                 <div className="name-bar--text">
                                     {character.characterClass}
                                 </div>
@@ -237,16 +117,15 @@ class TeamPreview extends React.Component<TeamPreviewProps, TeamPreviewState> {
                         </div>
                     ))}
                 </div> */}
-                {selectedTeam.length == teamSize ?
-                    activeCharacter ?
-                        <button onClick={this.submitTeam} className="btn btn--green w-50 hpx-100">
-                            SUBMIT TEAM
-                        </button> : 
-                    <h1 className="page-title">Pick your Lead Character</h1>
-                    :
-                    <h1 className="page-title">Select your team members ({selectedTeam.length} / {teamSize})</h1>
-                }
-            </div>
+                // {selectedTeam.length == teamSize ?
+                //     activeCharacter ?
+                //         <button onClick={this.submitTeam} className="btn btn--green w-50 hpx-100">
+                //             SUBMIT TEAM
+                //         </button> : 
+                //     <h1 className="page-title">Pick your Lead Character</h1>
+                //     :
+                //     <h1 className="page-title">Select your team members ({selectedTeam.length} / {teamSize})</h1>
+                // }
         )
     }
 }
